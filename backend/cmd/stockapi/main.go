bebe706b7054692ae834173c2e835d1637119db2
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-04-23 08:19:24 -0500

    Test functionality in main app

--- backend/cmd/stockapi/main.go
+++ backend/cmd/stockapi/main.go
@@ -1,25 +1,62 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/CorreaJose13/StockAPI/config"
 	"github.com/CorreaJose13/StockAPI/internal/api"
+	"github.com/CorreaJose13/StockAPI/internal/db"
+	"github.com/CorreaJose13/StockAPI/internal/repository"
+	"github.com/CorreaJose13/StockAPI/utils"
 )
 
 func main() {
+	ctx := context.Background()
 
 	cfg, err := config.LoadConfig()
 	if err != nil {
-		log.Fatalf("Failed to load configuration: %v", err)
+		log.Fatalf("failed to load configuration: %v", err)
 	}
+
+	repo, err := db.NewPostgresRepository(cfg)
+	if err != nil {
+		log.Fatalf("failed to initialize database repository: %v", err)
+	}
+
+	repository.SetStockRepository(repo)
+
+	defer repository.Close()
 
 	consumer := api.NewApiConsumer(cfg)
 
-	log.Println("Fetching stocks from API...")
+	log.Println("fetching stocks from API...")
 	stocks, err := consumer.FetchStocks()
 	if err != nil {
-		log.Fatalf("Failed to fetch stocks: %v", err)
+		log.Fatalf("failed to fetch stocks: %v", err)
 	}
-	log.Printf("Successfully fetched %d stocks", len(stocks))
+
+	log.Printf("successfully fetched %d stocks", len(stocks))
+
+	log.Println("Storing stocks in database...")
+	for _, stock := range stocks {
+		formattedStock, err := utils.Formatter(&stock)
+		if err != nil {
+			log.Printf("error formatting stock %s: %v", stock.Ticker, err)
+		}
+
+		if err := repository.InsertStock(ctx, formattedStock); err != nil {
+			log.Printf("error storing stock %s: %v", stock.Ticker, err)
+			continue
+		}
+	}
+
+	log.Println("Successfully stored stocks in database")
+
+	getStocks, err := repository.GetStocks(ctx)
+	if err != nil {
+		log.Fatalf("failed to get stocks: %v", err)
+	}
+
+	log.Printf("successfully retrieved %d stocks from database", len(getStocks))
 }
