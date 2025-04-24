0a01bc53c541ea9ab8105e3f89ad5f5e8d2c8b20
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-04-24 18:37:58 -0500

    Update gitignore and translated background color from dashboard to App.vue

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -46,35 +46,28 @@ const useStockData = computed(() => {
 })
 </script>
 <template>
-  <div class="min-h-screen bg-gray-100">
-    <div class="flex flex-col w-full max-w-screen-xl mx-auto">
-      <section class="flex flex-col gap-1 w-full py-4">
-        <h1 class="text-3xl font-bold text-start text-black">Dashboard</h1>
-        <p class="text-start text-gray-600">
-          Welcome to the dashboard! Here you can find all the information you need.
-        </p>
-      </section>
-      <section class="flex flex-col w-full md:flex-row justify-between">
-        <InfoCard title="Total Users" :value="12" color="text-black" description="Total number " />
-        <InfoCard
-          title="Total Users"
-          :value="12"
-          color="text-green-500"
-          description="Total number "
-        />
-        <InfoCard
-          title="Total Users"
-          :value="12"
-          color="text-red-600"
-          description="Total number "
-        />
-      </section>
-      <DashboardSection
-        title="Recommendations"
-        link="/stock-recommendations"
-        :stocks="useStockData"
+  <div class="flex flex-col w-full max-w-screen-xl mx-auto">
+    <section class="flex flex-col gap-1 w-full py-4">
+      <h1 class="text-3xl font-bold text-start text-black">Dashboard</h1>
+      <p class="text-start text-gray-600">
+        Welcome to the dashboard! Here you can find all the information you need.
+      </p>
+    </section>
+    <section class="flex flex-col w-full md:flex-row justify-between">
+      <InfoCard title="Total Users" :value="12" color="text-black" description="Total number " />
+      <InfoCard
+        title="Total Users"
+        :value="12"
+        color="text-green-500"
+        description="Total number "
       />
-      <DashboardSection title="Stocks" link="/all-stocks" :stocks="useStockData" />
-    </div>
+      <InfoCard title="Total Users" :value="12" color="text-red-600" description="Total number " />
+    </section>
+    <DashboardSection
+      title="Recommendations"
+      link="/stock-recommendations"
+      :stocks="useStockData"
+    />
+    <DashboardSection title="Stocks" link="/all-stocks" :stocks="useStockData" />
   </div>
 </template>
