fa092c5ed31c861248f97c91ce5203c87f65872b
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-04-24 17:29:02 -0500

    Create stock type

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,12 @@
+export interface Stock {
+  id: number
+  ticker: string
+  targetFrom: number
+  targetTo: number
+  company: string
+  action: string
+  brokerage: string
+  ratingFrom: string
+  ratingTo: string
+  time: string
+}
