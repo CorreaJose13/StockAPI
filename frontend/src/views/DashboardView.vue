2b833566c38f31901f32dc31386425c2cf709af3
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-04-25 06:05:30 -0500

    Use reusable stock data in dashboard view

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,49 +1,8 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { useStockData } from '@/composables/stocks'
 import InfoCard from '@/components/InfoCard.vue'
 import DashboardSection from '@/components/DashboardSection.vue'
-
-const useStockData = computed(() => {
-  const stocks = [
-    {
-      id: 1,
-      ticker: 'AAPL',
-      targetFrom: 180.5,
-      targetTo: 215.75,
-      company: 'Apple Inc.',
-      action: 'upgraded by',
-      brokerage: 'Morgan Stanley',
-      ratingFrom: 'Hold',
-      ratingTo: 'Buy',
-      time: '2025-01-15T09:30:00.000Z',
-    },
-    {
-      id: 2,
-      ticker: 'MSFT',
-      targetFrom: 320.0,
-      targetTo: 375.25,
-      company: 'Microsoft Corporation',
-      action: 'reiterated by',
-      brokerage: 'Goldman Sachs',
-      ratingFrom: 'Buy',
-      ratingTo: 'Buy',
-      time: '2025-01-14T14:45:00.000Z',
-    },
-    {
-      id: 3,
-      ticker: 'TSLA',
-      targetFrom: 195.0,
-      targetTo: 165.5,
-      company: 'Tesla, Inc.',
-      action: 'downgraded by',
-      brokerage: 'JP Morgan',
-      ratingFrom: 'Neutral',
-      ratingTo: 'Sell',
-      time: '2025-01-13T11:15:00.000Z',
-    },
-  ]
-  return stocks
-})
+const stocks = useStockData()
 </script>
 <template>
   <div class="flex flex-col w-full max-w-screen-xl mx-auto">
@@ -63,11 +22,7 @@ const useStockData = computed(() => {
       />
       <InfoCard title="Total Users" :value="12" color="text-red-600" description="Total number " />
     </section>
-    <DashboardSection
-      title="Recommendations"
-      link="/stock-recommendations"
-      :stocks="useStockData"
-    />
-    <DashboardSection title="Stocks" link="/all-stocks" :stocks="useStockData" />
+    <DashboardSection title="Recommendations" link="/stock-recommendations" :stocks="stocks" />
+    <DashboardSection title="Stocks" link="/all-stocks" :stocks="stocks" />
   </div>
 </template>
