0463dbfd949db907e0d60df6a87ce4960e7542d3
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-04-30 12:26:37 -0500

    Delete outputs, deployment and stage resources from apigateway endpoint module

--- infra/modules/network/api_gateway_endpoint/main.tf
+++ infra/modules/network/api_gateway_endpoint/main.tf
@@ -36,28 +36,6 @@ resource "aws_lambda_permission" "this" {
   source_arn    = "${data.aws_api_gateway_rest_api.this.execution_arn}/${var.stage}/${var.method}/${var.path}"
 }
 
-resource "aws_api_gateway_deployment" "this" {
-  rest_api_id = data.aws_api_gateway_rest_api.this.id
-
-  triggers = {
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.this.id,
-      aws_api_gateway_method.this.id,
-      aws_api_gateway_integration.this.id,
-    ]))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_api_gateway_stage" "this" {
-  deployment_id = aws_api_gateway_deployment.this.id
-  rest_api_id   = data.aws_api_gateway_rest_api.this.id
-  stage_name    = var.stage
-}
-
 // TO DO: Add CORS configuration
 # resource "aws_api_gateway_method" "options" {
 #   rest_api_id   = data.aws_api_gateway_rest_api.this.id
