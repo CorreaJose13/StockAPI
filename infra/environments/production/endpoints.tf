b119813d0edc6e152461cb8959c2a605ba2a942f
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-05-01 07:45:48 -0500

    Integrate changes made in lambda api integration

--- infra/environments/production/endpoints.tf
+++ infra/environments/production/endpoints.tf
@@ -11,12 +11,14 @@ module "metrics_endpoint" {
 
   env_vars = { DB_URL = var.DB_URL }
 
-  endpoint_name    = "metrics"
-  api_gateway_name = var.api_gateway_name
-  parent_id        = module.api_gateway.root_resource_id
-  endpoint_path    = "metrics"
-  http_method      = "GET"
-  stage            = var.stage
+  endpoint_name     = "metrics"
+  rest_api_id       = module.api_gateway.id
+  rest_api_exec_arn = module.api_gateway.execution_arn
+  parent_id         = module.api_gateway.root_resource_id
+  endpoint_path     = "metrics"
+  http_method       = "GET"
+  stage             = var.stage
+
 }
 
 module "analyze_endpoint" {
@@ -29,12 +31,13 @@ module "analyze_endpoint" {
   log_retention_days = 7
   env_vars           = { DB_URL = var.DB_URL }
 
-  endpoint_name    = "analyze"
-  api_gateway_name = var.api_gateway_name
-  parent_id        = module.api_gateway.root_resource_id
-  endpoint_path    = "analyze"
-  http_method      = "GET"
-  stage            = var.stage
+  endpoint_name     = "analyze"
+  rest_api_id       = module.api_gateway.id
+  rest_api_exec_arn = module.api_gateway.execution_arn
+  parent_id         = module.api_gateway.root_resource_id
+  endpoint_path     = "analyze"
+  http_method       = "GET"
+  stage             = var.stage
 }
 
 module "stocks_endpoint" {
@@ -42,17 +45,19 @@ module "stocks_endpoint" {
   lambda_source_path = "${path.module}/../../../backend/internal/functions/stocks/main.go"
   s3_bucket          = module.lambda_bucket.bucket
   lambda_role        = module.lambda_role.arn
-  timeout            = 10
+  timeout            = 12
   memory_size        = 128
   log_retention_days = 7
   env_vars           = { DB_URL = var.DB_URL }
 
-  endpoint_name    = "stocks"
-  api_gateway_name = var.api_gateway_name
-  parent_id        = module.api_gateway.root_resource_id
-  endpoint_path    = "list"
-  http_method      = "GET"
-  stage            = var.stage
+  endpoint_name     = "stocks"
+  rest_api_id       = module.api_gateway.id
+  rest_api_exec_arn = module.api_gateway.execution_arn
+  parent_id         = module.api_gateway.root_resource_id
+  endpoint_path     = "stocks"
+  http_method       = "GET"
+  stage             = var.stage
+
 }
 
 // TO DO: Improve redeployment strategy
