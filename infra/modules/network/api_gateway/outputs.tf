0d54b755d5f038bc79072da2bf770115f4e0fc19
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-05-01 07:43:41 -0500

    Add exc arn output

--- infra/modules/network/api_gateway/outputs.tf
+++ infra/modules/network/api_gateway/outputs.tf
@@ -2,6 +2,10 @@ output "id" {
   value = aws_api_gateway_rest_api.this.id
 }
 
+output "execution_arn" {
+  value = aws_api_gateway_rest_api.this.execution_arn
+}
+
 output "root_resource_id" {
   value = aws_api_gateway_rest_api.this.root_resource_id
 }
