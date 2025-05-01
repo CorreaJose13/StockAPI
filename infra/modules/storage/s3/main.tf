9fea5b7b3d6c2e56f4199bb729778607cfbd50c8
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-05-01 03:25:31 -0500

    Force bucket versioning

--- infra/modules/storage/s3/main.tf
+++ infra/modules/storage/s3/main.tf
@@ -6,6 +6,6 @@ resource "aws_s3_bucket" "this" {
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
-    status = var.versioning ? "Enabled" : "Suspended"
+    status = "Enabled"
   }
 }
