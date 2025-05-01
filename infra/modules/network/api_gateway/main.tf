ac711c979e9cc810aaa10846b5c45e7b2bc56d36
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-05-01 03:29:23 -0500

    Add root resource as output and apigateway log group

--- infra/modules/network/api_gateway/main.tf
+++ infra/modules/network/api_gateway/main.tf
@@ -3,14 +3,12 @@ resource "aws_api_gateway_rest_api" "this" {
   description = var.description
 }
 
+resource "aws_cloudwatch_log_group" "api_gateway" {
+  name              = "/aws/apigateway/${var.name}"
+  retention_in_days = var.log_retention_days
+}
+
 // TO DO: For improvement stage
-
-# resource "aws_cloudwatch_log_group" "api_gateway" {
-#   name              = "/aws/apigateway/${var.name}"
-#   retention_in_days = var.log_retention_days
-#   tags              = var.tags
-# }
-
 # resource "aws_api_gateway_method_settings" "this" {
 #   rest_api_id = aws_api_gateway_rest_api.this.id
 #   stage_name  = aws_api_gateway_stage.this.stage_name
