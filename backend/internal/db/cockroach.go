c7592209650d8819b756eabf1e278787d60a3434
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-05-02 03:59:13 -0500

    Implement crdb tx, new params for pagination: order, field, search and param validation

--- backend/internal/db/cockroach.go
+++ backend/internal/db/cockroach.go
@@ -3,18 +3,46 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 
 	"context"
 
 	"github.com/CorreaJose13/StockAPI/config"
 	"github.com/CorreaJose13/StockAPI/models"
+	"github.com/cockroachdb/cockroach-go/v2/crdb"
 	"github.com/lib/pq"
-	_ "github.com/lib/pq"
+)
+
+var (
+	validFields = map[string]bool{
+		"ticker":      true,
+		"target_from": true,
+		"target_to":   true,
+		"company":     true,
+		"action":      true,
+		"brokerage":   true,
+		"rating_from": true,
+		"rating_to":   true,
+		"time":        true,
+	}
+
+	validOrders = map[string]bool{
+		"ASC":  true,
+		"DESC": true,
+	}
 )
 
 type CockRoachRepository struct {
 	db *sql.DB
 }
+
+const (
+	defaultPage  = 1
+	defaultLimit = 10
+	defaultField = "time"
+	defaultOrder = "ASC"
+	maxLimit     = 100
+)
 
 func NewPostgresRepository(cfg *config.Config) (*CockRoachRepository, error) {
 	db, err := sql.Open("postgres", cfg.DBURL)
@@ -41,46 +69,39 @@ func NewPostgresRepository(cfg *config.Config) (*CockRoachRepository, error) {
 	return &CockRoachRepository{db}, nil
 }
 
+func (repo *CockRoachRepository) execInTransaction(ctx context.Context, fn func(*sql.Tx) error) error {
+	return crdb.ExecuteTx(ctx, repo.db, nil, fn)
+}
+
 func (repo *CockRoachRepository) Close() error {
 	return repo.db.Close()
 }
 
 func (repo *CockRoachRepository) BulkInsertStocks(ctx context.Context, stocks []*models.FormattedStock) error {
-	txn, err := repo.db.Begin()
-	if err != nil {
-		return err
-	}
-
-	stmt, err := txn.Prepare(pq.CopyIn("stocks", "ticker", "target_from", "target_to",
-		"company", "action", "brokerage", "rating_from", "rating_to", "time"))
-	if err != nil {
-		return err
-	}
-
-	for _, stock := range stocks {
-		_, err = stmt.Exec(stock.Ticker, stock.TargetFrom, stock.TargetTo, stock.Company,
-			stock.Action, stock.Brokerage, stock.RatingFrom, stock.RatingTo, stock.Time)
+	return repo.execInTransaction(ctx, func(tx *sql.Tx) error {
+		stmt, err := tx.Prepare(pq.CopyIn("stocks", "ticker", "target_from", "target_to",
+			"company", "action", "brokerage", "rating_from", "rating_to", "time"))
 		if err != nil {
 			return err
 		}
-	}
+		defer stmt.Close()
 
-	_, err = stmt.Exec()
-	if err != nil {
-		return err
-	}
+		//formatTimeStamp
+		for _, stock := range stocks {
+			_, err = stmt.Exec(stock.Ticker, stock.TargetFrom, stock.TargetTo, stock.Company,
+				stock.Action, stock.Brokerage, stock.RatingFrom, stock.RatingTo, stock.Time)
+			if err != nil {
+				return err
+			}
+		}
 
-	err = stmt.Close()
-	if err != nil {
-		return err
-	}
+		_, err = stmt.Exec()
+		if err != nil {
+			return err
+		}
 
-	err = txn.Commit()
-	if err != nil {
-		return err
-	}
-
-	return nil
+		return nil
+	})
 }
 
 func (repo *CockRoachRepository) GetStocks(ctx context.Context) ([]*models.FormattedStock, error) {
@@ -98,10 +119,17 @@ func (repo *CockRoachRepository) GetStocks(ctx context.Context) ([]*models.Forma
 	return stocks, nil
 }
 
-func (repo *CockRoachRepository) GetStocksPaginated(ctx context.Context, page, limit int) ([]*models.FormattedStock, error) {
+func (repo *CockRoachRepository) GetStocksFiltered(ctx context.Context, field, order, search string, page, limit int) ([]*models.FormattedStock, error) {
+	page, limit = normalizePaginationParams(page, limit)
 	offset := (page - 1) * limit
-	query := "SELECT * FROM stocks ORDER BY ticker ASC LIMIT $1 OFFSET $2"
-	rows, err := repo.db.QueryContext(ctx, query, limit, offset)
+
+	query := generatePaginationQuery(field, order, search)
+	params := make([]any, 0)
+	searchParams := getSearchParams(search)
+	params = append(params, searchParams...)
+	params = append(params, limit, offset)
+
+	rows, err := repo.db.QueryContext(ctx, query, params...)
 	if err != nil {
 		return nil, fmt.Errorf("failed to query stocks: %w", err)
 	}
@@ -113,6 +141,88 @@ func (repo *CockRoachRepository) GetStocksPaginated(ctx context.Context, page, l
 	}
 
 	return stocks, nil
+}
+
+func (repo *CockRoachRepository) GetTableLength(ctx context.Context) (int, error) {
+	var count int
+	err := repo.db.QueryRowContext(ctx, "SELECT COUNT(*) FROM stocks").Scan(&count)
+	if err != nil {
+		return 0, fmt.Errorf("failed to get table length: %w", err)
+	}
+	return count, nil
+}
+
+func generatePaginationQuery(field, order, search string) string {
+	var pagination string
+	baseQuery := "SELECT * FROM stocks"
+	searchStm := buildSearchStatement(search)
+	orderStm := buildOrderStatement(field, order)
+
+	query := baseQuery
+	if searchStm != "" {
+		query += " " + searchStm
+	}
+	if orderStm != "" {
+		query += " " + orderStm
+	}
+
+	if searchStm != "" {
+		pagination = " LIMIT $2 OFFSET $3"
+	} else {
+		pagination = " LIMIT $1 OFFSET $2"
+	}
+
+	return query + pagination
+}
+
+func normalizePaginationParams(page, limit int) (int, int) {
+	if page <= 0 {
+		page = defaultPage
+	}
+	if limit <= defaultLimit {
+		limit = defaultLimit
+	}
+	if limit > maxLimit {
+		limit = maxLimit
+	}
+	return page, limit
+}
+
+func isValidField(field string) bool {
+	return validFields[strings.ToLower(field)]
+}
+
+func isValidOrder(order string) bool {
+	return validOrders[strings.ToUpper(order)]
+}
+
+func buildSearchStatement(search string) string {
+	if search == "" {
+		return ""
+	}
+	return "WHERE ticker ILIKE $1 OR company ILIKE $1 OR brokerage ILIKE $1"
+}
+
+func getSearchParams(search string) []any {
+	if search == "" {
+		return []any{}
+	}
+	return []any{"%" + search + "%"}
+}
+
+func buildOrderStatement(field, order string) string {
+	if !isValidField(field) {
+		field = defaultField
+	}
+
+	if !isValidOrder(order) {
+		order = defaultOrder
+	}
+
+	field = strings.ToLower(field)
+	order = strings.ToUpper(order)
+
+	return fmt.Sprintf("ORDER BY %s %s", field, order)
 }
 
 func scanRows(rows *sql.Rows) ([]*models.FormattedStock, error) {
