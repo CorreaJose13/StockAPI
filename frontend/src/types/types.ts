9445c50bed7f86ab0d174f7bb3745ae30c17fdec
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-05-02 06:44:01 -0500

    Add new types and constants

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,12 +1,35 @@
 export interface Stock {
-  id: number
   ticker: string
-  targetFrom: number
-  targetTo: number
+  target_from: number
+  target_to: number
   company: string
   action: string
   brokerage: string
-  ratingFrom: string
-  ratingTo: string
+  rating_from: string
+  rating_to: string
   time: string
+}
+
+export interface StockWithScore extends Stock {
+  score: number
+}
+
+export interface MetricsResponse {
+  total_stocks: number
+  positive_change: number
+  negative_change: number
+  no_change: number
+}
+
+export interface StockResponse {
+  stocks: Stock[]
+  length: number
+}
+
+export interface StockWithScoreResponse {
+  stocks: StockWithScore[]
+}
+
+export interface ErrorResponse {
+  message: string
 }
