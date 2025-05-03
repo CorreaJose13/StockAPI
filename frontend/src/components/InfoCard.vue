043414531b42af194fa7f2ab8dd2395267d788fe
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-05-02 21:10:24 -0500

    Add single prop to display only value in InfoCard

--- frontend/src/components/InfoCard.vue
+++ frontend/src/components/InfoCard.vue
@@ -16,12 +16,21 @@ const props = defineProps({
     type: String,
     required: true,
   },
+  single: {
+    type: Boolean,
+    default: false,
+  },
 })
 </script>
 <template>
   <article class="flex flex-col w-40 p-4 bg-white rounded-lg shadow-md">
     <span class="text-lg font-semibold text-gray-700">{{ props.title }}</span>
-    <span :class="['text-3xl font-bold', props.color]">{{ props.value }}</span>
-    <p class="text-xs mt-2 text-gray-700">{{ props.description }}</p>
+    <div v-if="props.single">
+      <span :class="['text-5xl font-bold', props.color]">{{ props.value }}</span>
+    </div>
+    <div v-else>
+      <span :class="['text-3xl font-bold', props.color]">{{ props.value }}</span>
+      <p class="text-xs mt-2 text-gray-700">{{ props.description }}</p>
+    </div>
   </article>
 </template>
