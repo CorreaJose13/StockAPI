7f8bd484cf3aee7b563ecc36182311d414cc54e1
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-05-06 09:28:02 -0500

    Add func dedicated to db setup and full setup

--- backend/internal/functions/setup.go
+++ backend/internal/functions/setup.go
@@ -2,7 +2,6 @@ package functions
 
 import (
 	"errors"
-	"os"
 
 	"github.com/CorreaJose13/StockAPI/config"
 	"github.com/CorreaJose13/StockAPI/internal/db"
@@ -10,17 +9,15 @@ import (
 )
 
 var (
-	ErrMissingDBURL = errors.New("db url cannot be empty")
+	ErrMissingDBURL       = errors.New("db url cannot be empty")
+	ErrMissingAPIURL      = errors.New("api url cannot be empty")
+	ErrMissingBearerToken = errors.New("bearer token cannot be empty")
 )
 
-func Setup() (*db.CockRoachRepository, error) {
-	dbString := os.Getenv("DB_URL")
-	if dbString == "" {
+func DBSetup() (*db.CockRoachRepository, error) {
+	cfg := config.LoadDbConfig()
+	if cfg.DBURL == "" {
 		return nil, ErrMissingDBURL
-	}
-
-	cfg := &config.Config{
-		DBURL: dbString,
 	}
 
 	repo, err := db.NewPostgresRepository(cfg)
@@ -31,4 +28,26 @@ func Setup() (*db.CockRoachRepository, error) {
 	repository.SetStockRepository(repo)
 
 	return repo, nil
+}
+
+func FullSetup() (*db.CockRoachRepository, *config.Config, error) {
+	cfg := config.LoadLambdaConfig()
+	if cfg.DBURL == "" {
+		return nil, nil, ErrMissingDBURL
+	}
+	if cfg.APIURL == "" {
+		return nil, nil, ErrMissingAPIURL
+	}
+	if cfg.BearerToken == "" {
+		return nil, nil, ErrMissingBearerToken
+	}
+
+	repo, err := db.NewPostgresRepository(cfg)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	repository.SetStockRepository(repo)
+
+	return repo, cfg, nil
 }
