c24e921761f5be5b7b76a21ce8183664b26aa538
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-05-08 09:20:09 -0500

    Create MainHeader component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,44 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import { computed } from 'vue'
-import logo from '@/assets/logo.svg'
-
-const navItems = computed(() => {
-  return [
-    { name: 'Dashboard', path: '/' },
-    { name: 'Stocks', path: '/all-stocks' },
-    { name: 'Insights', path: '/stock-insights' },
-  ]
-})
+import { RouterView } from 'vue-router'
+import MainHeader from '@/components/MainHeader.vue'
 </script>
 <template>
-  <div class="flex flex-col min-h-screen">
+  <div class="flex min-h-screen flex-col font-mono">
     <header>
-      <div class="w-full text-black bg-white">
-        <div
-          class="flex flex-col max-w-screen-2xl mx-auto md:items-center md:justify-between md:flex-row"
-        >
-          <div class="flex flex-row items-center justify-between p-4">
-            <img :src="logo" alt="Logo" class="h-8 w-auto mr-2" />
-            <RouterLink class="text-xl font-semibold flex" to="/">
-              <p class="text-blue-900">Stock</p>
-              <p class="text-blue-500">Wise</p>
-            </RouterLink>
-          </div>
-          <nav class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
-            <RouterLink
-              v-for="nav in navItems"
-              :key="nav.name"
-              :to="nav.path"
-              class="px-2 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-              >{{ nav.name }}</RouterLink
-            >
-          </nav>
-        </div>
-      </div>
+      <MainHeader />
     </header>
-
-    <main class="flex-grow bg-gradient-to-r from-blue-900 via-blue-700 to-blue-800 pb-20">
+    <main class="flex-grow bg-black px-6 pb-4">
       <RouterView />
     </main>
   </div>
---
+++ frontend/src/components/MainHeader.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { computed } from 'vue'
+import logo from '@/assets/logo.svg'
+
+const navItems = computed(() => {
+  return [
+    { name: 'Stocks', path: '/all-stocks' },
+    { name: 'Insights', path: '/stock-insights' },
+  ]
+})
+</script>
+<template>
+  <div class="w-full bg-stone-900 px-6 py-4">
+    <div class="mx-auto flex max-w-screen-2xl flex-row items-center justify-between">
+      <div class="flex flex-row items-center gap-4">
+        <img :src="logo" alt="Logo" class="h-12 w-auto" />
+        <RouterLink class="text-2xl font-bold" to="/">
+          <p class="text-white">StockWise</p>
+        </RouterLink>
+      </div>
+      <nav class="flex flex-row gap-4">
+        <RouterLink
+          v-for="nav in navItems"
+          :key="nav.name"
+          :to="nav.path"
+          class="p-2 text-slate-200 hover:bg-gray-200 hover:text-black focus:bg-gray-200 focus:text-black"
+          >{{ nav.name }}</RouterLink
+        >
+      </nav>
+    </div>
+  </div>
+</template>
