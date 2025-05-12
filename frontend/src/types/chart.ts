818bbbb2897f20fe232a756e34fc5b51949a35e0
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-05-12 09:26:24 -0500

    Modify chart types

--- frontend/src/types/chart.ts
+++ frontend/src/types/chart.ts
@@ -1,44 +1,12 @@
-export interface ChartDataset {
-  label: string
-  fill: boolean
-  borderColor: string
-  yAxisID: string
-  tension: number
-  data: number[]
-}
-
 export interface ChartData {
-  labels: string[]
-  datasets: ChartDataset[]
+  open: number
+  high: number
+  low: number
+  close: number
+  volume: number
+  date: string
 }
 
-export interface ScaleOptions {
-  type?: string
-  display?: boolean
-  position?: string
-  ticks: {
-    color: string
-  }
-  grid: {
-    color: string
-    drawOnChartArea?: boolean
-  }
-}
-
-export interface ChartOptions {
-  stacked: boolean
-  maintainAspectRatio: boolean
-  aspectRatio: number
-  plugins: {
-    legend: {
-      labels: {
-        color: string
-      }
-    }
-  }
-  scales: {
-    x: ScaleOptions
-    y: ScaleOptions
-    y1: ScaleOptions
-  }
+export interface ChartResponse {
+  time_series: ChartData[]
 }
